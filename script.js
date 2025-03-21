1dc39148ff5a34ea7fddafbcbb9d23e8b9d3519d
Akhil <konda.akhil456@gmail.com>
2025-03-21 22:58:55 +0530

    added favicon and optimised code

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
+function loaderAnimation() {
 
 
-let tl = gsap.timeline()
-tl.from('.line h1', {
-  y: 150,
-  stagger: 0.3,
-  delay: 0.4,
-  duration: 0.7
-})
-tl.from('.timer, .line h2', {
-  opacity: 0,
-  onStart: function () {
-    let timer = document.querySelector('.timer h5');
-    let count = '';
-    setInterval(function () {
-      if (count <= 100) {
-        timer.textContent = count;
-        count++;
-      } else { }
-    }, 37);
-  }
-})
+  let tl = gsap.timeline()
+  tl.from('.line h1', {
+    y: 150,
+    stagger: 0.3,
+    delay: 0.4,
+    duration: 0.7
+  })
+  tl.from('.timer, .line h2', {
+    opacity: 0,
+    onStart: function () {
+      let timer = document.querySelector('.timer h5');
+      let count = '';
+      setInterval(function () {
+        if (count <= 100) {
+          timer.textContent = count;
+          count++;
+        } else { }
+      }, 37);
+    }
+  })
 
-tl.to('.loader', {
-  opacity: 0,
-  duration: 0.5,
-  delay: 3
-})
+  tl.to('.loader', {
+    opacity: 0,
+    duration: 0.5,
+    delay: 3
+  })
 
-tl.from('.page-1', {
-  y: 1600,
-  ease: Power4,
-  delay: 0.3,
-  opacity: 0,
-  duration: 0.4
-})
+  tl.from('.page-1', {
+    y: 1600,
+    ease: Power4,
+    delay: 0.3,
+    opacity: 0,
+    duration: 0.4
+  })
 
-tl.to('.loader', {
-  display: 'none'
-})
+  tl.to('.loader', {
+    display: 'none'
+  })
 
 
+
+}
+
+loaderAnimation();
