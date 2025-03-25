5a1507690494c44d7872d2f3b2043e914a08d606
Akhil <konda.akhil456@gmail.com>
2025-03-26 01:11:07 +0530

    page 3 80% completed

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function loaderAnimation() {
   tl.to('.loader', {
     opacity: 0,
     duration: 0.5,
-    delay: 0
+    delay: 3
   })
 
   tl.from('.page-1', {
@@ -101,9 +101,17 @@ function magnetelem() {
   });
 }
 
+function sheryAnimation() {
+  Shery.imageEffect('.image-div', {
+    style: 5,
+    config: { "a": { "value": 1.15, "range": [0, 30] }, "b": { "value": -0.94, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749932567818 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.24, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": false }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.46, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 7.63, "range": [0, 100] } },
+    gooey: true,
+  });
+
+}
+
 locomotivescroll();
 magnetelem();
 customCursor();
 loaderAnimation();
-
-
+sheryAnimation();
