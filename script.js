77a7def0ff55cd57a97f04f34061b58d5a149492
Akhil <konda.akhil456@gmail.com>
2025-03-27 16:20:28 +0530

    responsive ness pending

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function loaderAnimation() {
   tl.to('.loader', {
     opacity: 0,
     duration: 0.5,
-    delay: 3
+    delay: 0
   })
 
   tl.from('.page-1', {
@@ -86,20 +86,80 @@ function loaderAnimation() {
 
 }
 function customCursor() {
-  document.addEventListener("mousemove", function (dets) {
-    gsap.to('.cursor', {
-      x: dets.x,
-      y: dets.y
-    })
-  })
-}
-function magnetelem() {
-  Shery.makeMagnet(".nav-2 h6" /* Element to target.*/, {
-    //Parameters are optional.
+  // document.addEventListener("mousemove", function (dets) {
+  //   gsap.to('.cursor', {
+  //     x: dets.x,
+  //     y: dets.y
+  //   })
+  //   let videoContainer = document.querySelector('.video1');
+  //   videoContainer.addEventListener('mouseenter', () => {
+  //     videoContainer.addEventListener('mousemove', (dets) => {
+  //       gsap.to('.video-cursor', {
+  //         x: dets.x - 1300,
+  //         y: dets.y - 190
+  //       })
+  //     })
+  //   })
+
+
+  // })
+  Shery.mouseFollower({
+    skew: true,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
-}
+  Shery.makeMagnet('.nav-2 h6');
+
+  let videoContainer = document.querySelector('.video1');
+  let video = document.querySelector('.video1 video');
+
+  let flag = 0;
+  video.addEventListener('click', () => {
+    if (flag == 0) {
+
+      video.play()
+      video.style.opacity = 1
+      document.querySelector('.video-cursor').innerHTML = '<i class="ri-pause-large-fill"></i>';
+      gsap.to('.video-cursor', {
+        scale: 0.5,
+      })
+      flag = 1
+
+    } else {
+
+      video.pause()
+      video.style.opacity = 0
+      document.querySelector('.video-cursor').innerHTML = '<i class="ri-play-large-fill"></i>';
+      gsap.to('.video-cursor', {
+        scale: 1,
+      })
+      flag = 0;
+    }
+  })
+
+  videoContainer.addEventListener('mouseenter', () => {
+    gsap.to('.mousefollower', {
+      opacity: 0,
+    });
+    videoContainer.addEventListener('mousemove', (dets) => {
+      gsap.to('.video-cursor', {
+        x: dets.x - 1300,
+        y: dets.y - 190
+      });
+    });
+  });
+  videoContainer.addEventListener('mouseleave', () => {
+    gsap.to('.mousefollower', {
+      opacity: 1,
+    });
+    videoContainer.addEventListener('mouseleave', () => {
+      gsap.to('.video-cursor', {
+        x: '70%',
+        y: '-15%',
+      });
+    });
+  });
+};
 
 function sheryAnimation() {
   Shery.imageEffect('.image-div', {
@@ -110,8 +170,38 @@ function sheryAnimation() {
 
 }
 
-locomotivescroll();
-magnetelem();
-customCursor();
+// function magnetelem() {
+//   Shery.makeMagnet(".nav-2 h6" /* Element to target.*/, {
+//     //Parameters are optional.
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
+// }
+
+// magnetelem();
 loaderAnimation();
+customCursor();
+locomotivescroll();
 sheryAnimation();
+
+
+
+
+document.addEventListener('mousemove', (dets) => {
+  gsap.to('.flag-india', {
+    x: dets.x,
+    y: dets.y
+  })
+})
+
+document.querySelector('#hero3').addEventListener('mouseenter', () => {
+  gsap.to('.flag-india', {
+    opacity: 1
+  })
+})
+document.querySelector('#hero3').addEventListener('mouseleave', () => {
+  gsap.to('.flag-india', {
+    opacity: 0
+  })
+})
+
